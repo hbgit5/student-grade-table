feea5b720bcaa7e4b20bd2f2f358a99ff6a4757f
Thi Nguyen <thiman@gmail.com>
2014-12-11 13:18:30 -0800

    Step 3

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // Step 1: Add data to students array
 // Step 2: Add TR row to table
+// Step 3: Everything else
 var students = [
     //{fname: "Joe", lname: "Schmoe", grade: 55}
 ];
 
-var fname_field, lname_field, grade_field, add_btn;
+var high_index = 0;
+var low_index = 0;
+
+var fname_field, lname_field, grade_field, add_btn, del_btn;
 
 function init() {
     // Assign references to various elements
@@ -15,6 +19,18 @@ function init() {
     
     // Create click event for add student button
     add_btn.click(addStudent);
+    
+    $('tbody').on('click', '.btn-del-student', removeStudent);
+}
+
+function removeStudent() {
+    var row_index = $(this).parent().parent().index();
+    $(this).parent().parent().remove();
+    
+    students.splice(row_index, 1);
+    
+    updateAverage();
+    compareGrades();
 }
 
 function addStudent() {
@@ -30,13 +46,15 @@ function addStudent() {
     
     // Add student object into students array
     students.push(student);
+    console.log(students);
     
     // Clear out fields
     fname_field.val('');
     lname_field.val('');
     grade_field.val('');
     
-    console.log(students);
+    updateAverage();
+    compareGrades();
 }
 
 function addRow(student) {
@@ -44,12 +62,52 @@ function addRow(student) {
     var td_name = $('<td>').html(student.fname + ' ' + student.lname);
     var td_grade = $('<td>').html(student.grade);
     
-    var button = $('<button>').addClass('btn btn-danger').html('X');
+    var button = $('<button>').addClass('btn btn-danger btn-del-student').html('X');
     var td_button = $('<td>').append(button);
     
     tr.append(td_name).append(td_grade).append(td_button);
     
     $('#grades-table tbody').append(tr);
+}
+
+function updateAverage() {
+    $('#result').html(calculateAverage());
+}
+
+function calculateAverage() {
+    var sum = 0;
+    for(var i = 0; i < students.length; i++) {
+        sum += students[i].grade;
+    }
+    
+    var avg = sum / students.length;
+    return avg;
+}
+
+function compareGrades() {
+    var high = 0;
+    var low = 9999;
+    var rows = $('tbody tr');
+    
+    $(rows[high_index]).removeClass('bg-info');
+    $(rows[low_index]).removeClass('bg-danger');
+    
+    for(var i = 0; i < students.length; i++) {
+        if(students[i].grade < low) {
+            low = students[i].grade;
+            low_index = i;
+        }
+        
+        if(students[i].grade > high) {
+            high = students[i].grade;
+            high_index = i;
+        }
+    }
+    
+    $(rows[high_index]).addClass('bg-info');
+    if(students.length > 1) {
+        $(rows[low_index]).addClass('bg-danger');
+    }
 }
 
 $(document).ready(init);
